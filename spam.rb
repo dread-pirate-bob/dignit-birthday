0f7b64b18b417e674ebcdc0a28b9a290b4af9066
George <guno@ishoutout.com>
2014-06-23 16:19:14 -0700

    next step

--- spam.rb
+++ spam.rb
@@ -1,10 +1,13 @@
 puts "What's your name?"
 name = gets.strip!
+greetPerson(name)
 
-if name == "Jim"
-	puts "Hello #{name}"
-elsif name == "Nick"
-	puts "Go away #{name}"
-else
-	puts "Hello #{name}, it's nice to meet you!"
+def greetPerson (name)
+	if name == "Jim"
+		puts "Hello #{name}"
+	elsif name == "Nick"
+		puts "Go away #{name}"
+	else
+		puts "Hello #{name}, it's nice to meet you!"
+	end
 end
